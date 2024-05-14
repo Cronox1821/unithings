3c8c9a8cbc81daea874d99882b66cbc42f7ca6b5
Cronox1821 <fabichucky3@gmail.com>
2024-05-14 09:52:30 -0400

    rut / verificacion rut, cambio en pattern

--- Python/ComplejoEducacional/actFabianMaulen_chile2030.py
+++ Python/ComplejoEducacional/actFabianMaulen_chile2030.py
@@ -1,9 +1,24 @@
 # Autor: Fabian Gabriel Maulen Maulen
+def calcularRut(rut):
+    for char in rut:
+        pass
+
+def verificarRepeticion(num):
+    for char in num:
+        contador = 0
+        for i in len.num()-1:
+            if char == num[i+1]:
+                contador += 1
+        if contador >= 4:
+            return False
+
+
 import re
 nombres = []
 direcciones = []
 telefonos = []
-dir_pattern = "[A-Za-z]+ +[0-9]+ +[A-Za-z0-9]+ +[0-9]+ +[A-Za-z]"
+ruts = []
+dir_pattern = "[A-Za-z]+ +[0-9]"
 add = ''
 while add != 'n':
     try:
@@ -20,14 +35,22 @@ while add != 'n':
                 telefonos.append(num)
                 print(telefonos[-1])
                 print(len(telefonos[-1]))
-                if len(num) < 8:
-                    print(f'Longitud del numero "{telefonos.pop(-1)}" invalida, asegurese de ingresar 8 digitos')
+                if len(num) != 8 and not num.isdigit():
+                    print(f'Longitud del numero "{telefonos.pop(-1)}" invalida, asegurese de que el numero sea de 8 digitos')
                 else:
                     print('Numero agregado')
                     break
             except ValueError:
                 del telefonos[-1]
                 print('Numero ingresado invalido, se ha detectado un caracter, intente nuevamente ingresando solo numeros')
+        #while True:
+        #    ruts.append(input('Ingrese rut sin puntos y sin guion: '))
+        #    if 8 >= len(ruts[-1]) >= 9:
+        #        
+        #        break
+        #    else:
+        #        print('Rut invalido, intente nuevamente')
+
         while True:
             add = input('Desea agregar otro alumno (s/n): ')
             if add.lower() == "s" or add.lower() == "n":
